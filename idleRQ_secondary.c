dc81e0370506e7d5f5f399faf4edd08c5770e272
Wada <wadamc@UMICH.EDU>
2026-03-27 10:48:26 -0400

    Create Secondary file

---
+++ idleRQ_secondary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+
+int main(void)
+{
+    return 0;
+}
+
+
